d30f54e06d86e30a35c6560a6e2212eaab623ee9
Scott Bedard <scottbedard90@gmail.com>
2024-12-03 21:54:19 -0600

    minified build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'rollup'
+import { minify } from 'rollup-plugin-esbuild-minify'
+import pkg from './package.json' with { type: 'json' }
 import typescript from '@rollup/plugin-typescript'
 
 export default defineConfig({
   input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'es',
+    },
+    {
+      file: pkg.unpkg,
+      format: 'iife',
+      name: 'Hexchess',
+      plugins: [
+        minify(),
+      ],
+      sourcemap: true,
+    },
+  ],
   plugins: [
     typescript(),
   ],
