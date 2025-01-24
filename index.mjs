7d42eba2e8e73012d3e151bf7b313968bb80f5b6
Scott Bedard <scottbedard90@gmail.com>
2025-01-23 20:50:18 -0600

    cleanup

--- index.mjs
+++ index.mjs
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
 import cli from './dist/cli.mjs'
 
-const output = cli(process.argv.slice(2))
-
-if (output) {
-  console.log(output)
-}
+console.log(cli(process.argv.slice(2)))
