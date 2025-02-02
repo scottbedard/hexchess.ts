8fa6fcba3961197d95e981259de9a88592394d7c
Scott Bedard <scottbedard90@gmail.com>
2025-02-02 16:27:16 -0600

    coverage

--- tests/cli.spec.ts
+++ tests/cli.spec.ts
@@ -17,6 +17,11 @@ describe('apply', () => {
     const output = cli(['apply', initialPosition, 'c2c4'])
     expect(output).toBe('b/qbk/n1b1n/r5r/ppppppppp/11/5P5/2P1P1P4/3P1B1P3/5B2P2/1PRNQBKNRP1 b c3 0 1')
   })
+
+  test('help', () => {
+    const output = cli(['apply', '-h'])
+    expect(typeof output).toBe('string')
+  })
 })
 
 describe('is-checkmate', () => {
@@ -28,6 +33,11 @@ describe('is-checkmate', () => {
     const output = cli(['is-checkmate', 'K/3/2q2/3q3/9/11/11/11/11/11/11 w - 1 2'])
     expect(output).toBe('true')
   })
+
+  test('help', () => {
+    const output = cli(['is-checkmate', '-h'])
+    expect(typeof output).toBe('string')
+  })
 })
 
 describe('is-stalemate', () => {
@@ -38,6 +48,11 @@ describe('is-stalemate', () => {
   test('success', () => {
     const output = cli(['is-stalemate', 'k/1P1/2K2/7/9/11/11/11/11/11/11 b - 1 1'])
     expect(output).toBe('true')
+  })
+
+  test('help', () => {
+    const output = cli(['is-stalemate', '-h'])
+    expect(typeof output).toBe('string')
   })
 })
 
@@ -54,6 +69,11 @@ describe('moves', () => {
     const output = cli(['moves', initialPosition, 'e4'])
     expect(output).toBe('e4e5,e4e6')
   })
+
+  test('help', () => {
+    const output = cli(['moves', '-h'])
+    expect(typeof output).toBe('string')
+  })
 })
 
 describe('parse', () => {
@@ -65,6 +85,11 @@ describe('parse', () => {
     const output = cli(['parse', initialPosition])
     expect(output).toEqual(JSON.stringify(new Hexchess(initialPosition)))
   })
+
+  test('help', () => {
+    const output = cli(['parse', '-h'])
+    expect(typeof output).toBe('string')
+  })
 })
 
 describe('stringify', () => {
@@ -75,6 +100,11 @@ describe('stringify', () => {
   test('success', () => {
     const output = cli(['stringify', JSON.stringify(new Hexchess(initialPosition))])
     expect(output).toBe(initialPosition)
+  })
+
+  test('help', () => {
+    const output = cli(['stringify', '-h'])
+    expect(typeof output).toBe('string')
   })
 })
 
